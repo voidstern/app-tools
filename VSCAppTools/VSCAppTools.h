7be3cf9689577ba9bb4cdccd88f18ecc6a9ac934
Lukas Burgstaller <mail@voidstern.net>
2017-05-14 10:12:26 +0200

    Initial Commit

---
+++ VSCAppTools/VSCAppTools.h
@@ -0,0 +1,19 @@
+//
+//  VSCAppTools.h
+//  VSCAppTools
+//
+//  Created by Lukas on 14/05/2017.
+//  Copyright © 2017 Lukas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VSCAppTools.
+FOUNDATION_EXPORT double VSCAppToolsVersionNumber;
+
+//! Project version string for VSCAppTools.
+FOUNDATION_EXPORT const unsigned char VSCAppToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VSCAppTools/PublicHeader.h>
+
+
