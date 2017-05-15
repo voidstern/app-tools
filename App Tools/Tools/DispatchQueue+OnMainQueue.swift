e789b7b2e055bbf28de6431840aca2daf06b75fa
Lukas Burgstaller <mail@voidstern.net>
2017-05-15 15:44:56 +0200

    Add onMainQueue

---
+++ App Tools/Tools/DispatchQueue+OnMainQueue.swift
@@ -0,0 +1,19 @@
+//
+//  DispatchQueue+OnMainQueue.swift
+//  VSCAppTools
+//
+//  Created by Lukas on 15/05/2017.
+//  Copyright © 2017 Lukas. All rights reserved.
+//
+
+import Foundation
+
+extension DispatchQueue {
+    public static func onMainQueue(_ block: @escaping () -> ()) {
+        if Thread.isMainThread {
+            block()
+        } else {
+            DispatchQueue.main.async(execute: block)
+        }
+    }
+}
