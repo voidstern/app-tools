833379ef851662ce353f401aac203c3bbeb91130
Lukas Burgstaller <mail@voidstern.net>
2019-08-10 08:51:03 +0200

    Dark / Light Colors

--- App Tools/ColorManager.swift
+++ App Tools/ColorManager.swift
@@ -21,7 +21,7 @@ final public class ColorManager {
         }
 
         public var uiColor: UIColor {
-            return UIColor(rgba: ColorManager.shared.hexString(for: self))
+            return UIColor(color: self)
         }
 
         public static func == (lhs: Color, rhs: Color) -> Bool {
@@ -31,31 +31,74 @@ final public class ColorManager {
 
     public typealias ColorDef = [String: String]
 
-    private var colors: ColorDef = [:]
+    fileprivate var lightColors: ColorDef = [:]
+    fileprivate var darkColors: ColorDef = [:]
 
-    public func loadColors(_ colors: ColorDef) {
-        self.colors = colors
+    public func loadColors(light: ColorDef, dark: ColorDef) {
+        self.lightColors = light
+        self.darkColors = dark
+        
         NotificationCenter.default.post(name: ColorManager.colorsChangedNotificationName, object: self)
     }
 
     public func loadColors(json jsonURL: URL?) {
-        if let jsonURL = jsonURL, let data = try? Data(contentsOf: jsonURL), let colorsDict = ((try? JSONSerialization.jsonObject(with: data, options: []) as? ColorDef) as ColorManager.ColorDef??), let colors = colorsDict {
-            self.colors = colors
-            NotificationCenter.default.post(name: ColorManager.colorsChangedNotificationName, object: self)
-        } else {
-            print("Failed to load colors json dict for \(jsonURL?.absoluteString ?? "<nil>")")
+        loadColors(light: jsonURL, dark: jsonURL)
+    }
+
+    public func loadColors(light lightJSONURL: URL?, dark darkJSONURL: URL?) {
+        guard let lightJSONURL = lightJSONURL else {
+            return
         }
+        
+        guard let lightData = try? Data(contentsOf: lightJSONURL), let lightColorsDict = ((try? JSONSerialization.jsonObject(with: lightData, options: []) as? ColorDef) as ColorManager.ColorDef??), let lightColors = lightColorsDict else {
+            print("Failed to load light colors json dict for \(lightJSONURL.absoluteString)")
+            return
+        }
+        
+        let darkJSONURL = darkJSONURL ?? lightJSONURL
+        guard let darkData = try? Data(contentsOf: darkJSONURL), let darkColorsDict = ((try? JSONSerialization.jsonObject(with: darkData, options: []) as? ColorDef) as ColorManager.ColorDef??), let darkColors = darkColorsDict else {
+            print("Failed to load light colors json dict for \(darkJSONURL.absoluteString)")
+            return
+        }
+        
+        loadColors(light: lightColors, dark: darkColors)
     }
 
-    public func hexString(for color: Color) -> String {
-        return colors[color.name] ?? "#000000"
+    fileprivate func lightColor(for color: Color) -> UIColor {
+        return UIColor(rgba: lightColors[color.name] ?? "#FAFAFA")
     }
 
+    fileprivate func darkColor(for color: Color) -> UIColor {
+        return UIColor(rgba: darkColors[color.name] ?? "#232323")
+    }
 }
 
 extension UIColor {
-    
     public convenience init(color: ColorManager.Color) {
-        self.init(rgba: ColorManager.shared.hexString(for: color))
+        #if os(iOS)
+        let cm = ColorManager.shared
+        if #available(iOS 13.0, *) {
+            self.init(light: cm.lightColor(for: color), dark: cm.darkColor(for: color))
+        } else {
+            self.init(rgba: ColorManager.shared.lightColors[color.name] ?? "#FAFAFA")
+        }
+        #else
+        self.init(rgba: ColorManager.shared.lightColors[color.name] ?? "#FAFAFA")
+        #endif
     }
 }
+
+#if os(iOS)
+@available(iOS 13.0, *)
+extension UIColor {
+    convenience init(light: UIColor, dark: UIColor) {
+        self.init(dynamicProvider: { (traits) in
+            switch traits.userInterfaceStyle {
+            case .unspecified, .light: return light
+            case .dark: return dark
+            @unknown default: return light
+            }
+        })
+    }
+}
+#endif
