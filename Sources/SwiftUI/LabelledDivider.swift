fa37bc26b552cd296cfaeb1902bd86fc61ac2e1c
Lukas Burgstaller <mail@voidstern.net>
2022-08-09 00:09:47 +0200

    Added labelled divider

---
+++ Sources/SwiftUI/LabelledDivider.swift
@@ -0,0 +1,34 @@
+//
+//  LabelledDivider.swift
+//  WalkieTalkie
+//
+//  Created by Lukas Burgstaller on 05.07.22.
+//
+
+import Foundation
+import SwiftUI
+
+struct LabelledDivider: View {
+
+    let label: String
+    let horizontalPadding: CGFloat
+    let color: Color
+
+    init(label: String, horizontalPadding: CGFloat = 20, color: Color = .gray) {
+        self.label = label
+        self.horizontalPadding = horizontalPadding
+        self.color = color
+    }
+
+    var body: some View {
+        HStack {
+            line
+            Text(label).foregroundColor(color).bold()
+            line
+        }
+    }
+
+    var line: some View {
+        VStack { Divider().background(color) }.padding(horizontalPadding)
+    }
+}
