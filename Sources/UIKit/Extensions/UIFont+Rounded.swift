0e653a60b7bdc94cd4fc255ee8da0373b2fc9b5f
Lukas Burgstaller <mail@voidstern.net>
2023-02-16 21:27:56 +0100

    Added rounded font

---
+++ Sources/UIKit/Extensions/UIFont+Rounded.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lukas Burgstaller on 16.02.23.
+//
+
+import UIKit
+
+extension UIFont {
+    class func rounded(ofSize size: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let systemFont = UIFont.systemFont(ofSize: size, weight: weight)
+        let font: UIFont
+        
+        if let descriptor = systemFont.fontDescriptor.withDesign(.rounded) {
+            font = UIFont(descriptor: descriptor, size: size)
+        } else {
+            font = systemFont
+        }
+        return font
+    }
+}
