880390acddd84a5039938295bd77c018b58780a1
Lukas <mail@voidstern.net>
2023-11-12 16:17:29 +0100

    Added if modifier

---
+++ Sources/SwiftUI/IfModifier.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lukas Burgstaller on 07.11.23.
+//
+
+import Foundation
+import SwiftUI
+
+//@available(macOS 10.15, *)
+extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder
+    public func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
+
