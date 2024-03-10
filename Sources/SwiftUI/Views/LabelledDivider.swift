cb4224a3a8a80aeb3b2877bb1f0ae3abbd3beff0
Lukas <mail@voidstern.net>
2024-03-10 14:13:43 +0100

    public divider

--- Sources/SwiftUI/Views/LabelledDivider.swift
+++ Sources/SwiftUI/Views/LabelledDivider.swift
@@ -8,18 +8,18 @@
 import Foundation
 import SwiftUI
 
-struct LabelledDivider: View {
+public struct LabelledDivider: View {
     let label: String
     let horizontalPadding: CGFloat
     let color: Color
 
-    init(label: String, horizontalPadding: CGFloat = 20, color: Color = .gray) {
+    public init(label: String, horizontalPadding: CGFloat = 20, color: Color = .gray) {
         self.label = label
         self.horizontalPadding = horizontalPadding
         self.color = color
     }
 
-    var body: some View {
+    public var body: some View {
         HStack {
             line
             Text(label).foregroundColor(color).bold()
