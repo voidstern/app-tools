177168b4cc333bf030d4973f760500f9d43c3c07
Lukas <mail@voidstern.net>
2024-04-11 08:20:56 +0200

    if else modifier

--- Sources/AppTools/Modifiers/IfModifier.swift
+++ Sources/AppTools/Modifiers/IfModifier.swift
@@ -8,7 +8,6 @@
 import Foundation
 import SwiftUI
 
-//@available(macOS 10.15, *)
 extension View {
     /// Applies the given transform if the given condition evaluates to `true`.
     /// - Parameters:
@@ -21,6 +20,20 @@ extension View {
             transform(self)
         } else {
             self
+        }
+    }
+    
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder
+    public func `if`<Content: View, ElseContent: View>(_ condition: Bool, transform ifTransform: (Self) -> Content, else elseTransform: (Self) -> ElseContent) -> some View {
+        if condition {
+            ifTransform(self)
+        } else {
+            elseTransform(self)
         }
     }
 }
