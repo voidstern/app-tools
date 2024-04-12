87fdeb07c59d62864c0829d48eb21194be56e152
Lukas <mail@voidstern.net>
2024-04-12 11:36:05 +0200

    Fixed size issue

--- Sources/AppTools/Views/CachedAsyncImage.swift
+++ Sources/AppTools/Views/CachedAsyncImage.swift
@@ -406,5 +406,5 @@ private extension URLSession {
 }
 
 extension URLCache {
-    static let imageCache = URLCache(memoryCapacity: 512_000_000, diskCapacity: 10_000_000_000)
+    static let imageCache = URLCache(memoryCapacity: 512_000_000, diskCapacity: 1_000_000_000)
 }
